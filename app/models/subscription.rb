5384ec97ec731318efa90697fa14a7890aa8a124
Brian Augnoo <augnoobrian@gmail.com>
2025-05-29 22:40:08 +0400

    start refactoring subscription

--- app/models/subscription.rb
+++
@@ -1,4 +0,0 @@
-class Subscription < ApplicationRecord
-  belongs_to :user
-  has_many :subscribers, dependent: :destroy
-end
--- db/migrate/20250527165648_create_subscriptions.rb
+++
@@ -1,9 +0,0 @@
-class CreateSubscriptions < ActiveRecord::Migration[8.0]
-  def change
-    create_table :subscriptions do |t|
-      t.references :user, null: false, foreign_key: true
-
-      t.timestamps
-    end
-  end
-end
