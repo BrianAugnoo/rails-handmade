83a567a993dd30c5ff7a2044d47edd4b69ada897
Brian Augnoo <augnoobrian@gmail.com>
2025-06-01 23:22:26 +0400

    solid cable initit

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,8 @@
 class Message < ApplicationRecord
+  attr_accessor :current_user
   belongs_to :conversation
   belongs_to :user
+  after_create_commit :broadcast_create
 
   # always use this method to create a message in controller
   def save_in_conversation(params)
@@ -16,5 +18,13 @@ class Message < ApplicationRecord
         self.conversation = conversation
       end
     self.save ? true : false
+  end
+
+  private
+  def broadcast_create
+    broadcast_append_to "conversation_#{self.conversation.id}",
+                         target: "messages",
+                         partial: "conversations/message",
+                         locals: { message: self, position: (self.user == self.current_user ? "ms-auto" : "") }
   end
 end
