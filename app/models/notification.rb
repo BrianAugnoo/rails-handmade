db10cc31b834a34764bd528c43ab5ebe245e3e99
Brian Augnoo <augnoobrian@gmail.com>
2025-06-02 18:25:49 +0400

    succes

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,3 +1,22 @@
 class Notification < ApplicationRecord
   belongs_to :user
+
+  def message_notification(params)
+    self.user = params[:user]
+    if self.save
+      broadcast_notification(params[:recipient]) ? true : false
+    else
+      false
+    end
+  end
+
+  private
+
+  def broadcast_notification(recipient)
+    broadcast_append_to "notification_#{recipient.id}",
+                         target: "notifications",
+                         partial: "notifications/notification",
+                         locals: { notification: self }
+    true
+  end
 end
