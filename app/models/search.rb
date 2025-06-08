dd96951fb840eadecd9191f1ed6c2844b1cd3c23
Brian Augnoo <augnoobrian@gmail.com>
2025-06-08 19:05:27 +0400

    search model refactor

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,15 +1,14 @@
 class Search
-  def self.user(query)
-    User.where("user_name ILIKE ?", "%#{query}%").first(10)
+  def self.user(query, current_user)
+    users = User.where("user_name ILIKE ?", "%#{query}%")
+    result = { existing_conversation: [], inexsting_conversation: [] }
+    users.each do |user|
+      Conversation.between?(user, current_user) ? result[:existing_conversation] << Conversation.between(user, current_user) : result[:inexsting_conversation] << user
+    end
+    result
   end
 
   def self.art(query)
     Art.where("tags ILIKE ?", "%#{query}%").first(10)
-  end
-
-  def self.global(query)
-    users = self.user(query)
-    arts = self.art(query)
-    { users: users, arts: arts }
   end
 end
