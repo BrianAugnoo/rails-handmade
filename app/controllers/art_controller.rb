fd0ab4dcbd80bf937cf1ff18b01e03bf475c772f
Brian Augnoo <augnoobrian@gmail.com>
2025-06-11 15:13:44 +0400

    local test without cloudinary

--- app/controllers/art_controller.rb
+++ app/controllers/art_controller.rb
@@ -1,7 +1,21 @@
 class ArtController < ApplicationController
   def new
+    @art = Art.new
   end
 
   def create
+    @art = Art.new(set_params)
+    @art.user = current_user
+    if @art.save
+      redirect_to root_path, notice: "Art created successfully."
+    else
+      flash.now[:alert] = @art.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  private
+  def set_params
+    params.require(:art).permit(:description, :photo, :video, :tags)
   end
 end
