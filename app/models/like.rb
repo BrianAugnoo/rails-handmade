fb364db81b17be4d58f5e927967f07eb2ac5bedb
Brian Augnoo <augnoobrian@gmail.com>
2025-06-24 20:13:57 +0400

    fixed

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,14 +1,15 @@
 class Like < ApplicationRecord
   attr_accessor :current_user
+  attr_accessor :skip_broadcast
   belongs_to :user
   belongs_to :art
-  after_commit :create_broadcast
+  after_create_commit :create_broadcast, unless: -> { skip_broadcast }
 
   def self.liked?(user, art)
      exists?(user: user, art: art)
   end
 
-  def create_broadcast()
+  def create_broadcast
     broadcast_update_to "turbo_#{self.art.id}_likes",
                          target: "art_#{self.art.id}_likes",
                          partial: "home/like",
