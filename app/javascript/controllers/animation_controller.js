97b39e6d1f92dabd70b76fe55cf078a95860a3a3
Brian Augnoo <augnoobrian@gmail.com>
2025-06-26 20:28:52 +0400

    basic front

--- app/javascript/controllers/animation_controller.js
+++ app/javascript/controllers/animation_controller.js
@@ -2,6 +2,16 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="animation"
 export default class extends Controller {
+  static targets = ["dropItem"]
   connect() {
+    console.log("get out!")
+  }
+
+  dropdown(event) {
+    event.preventDefault()
+    console.log("drop")
+    const drop = this.dropItemTarget.hidden
+    console.log(drop)
+    this.dropItemTarget.hidden = drop ? false : true
   }
 }
